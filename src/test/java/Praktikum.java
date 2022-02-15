9248f36ac9c1d40b765aa39608938dab64121919
Сергей Кулик <qlike@Mini.local>
2022-02-15 22:04:03 +0300

    создание первого коммита

---
+++ src/test/java/Praktikum.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Praktikum {
+
+    @Test
+    public void test() {
+
+        // создай драйвер для браузера Chrome        
+        // перейди на страницу тестового стенда
+        ...
+        // выполни авторизацию
+        ...
+        // получи текст второй карточки и сохрани его в переменную cardText
+        ... cardText = ...
+
+    }
+}
